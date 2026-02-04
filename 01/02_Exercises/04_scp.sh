0c2b5ce17cc68dedc4c25b063d25a1078e7cf46c
im23b-terenzianie <terenzianie@bzz.ch>
2026-02-04 11:25:55 +0100

    modified:   01/02_Exercises/04_scp.sh modified:   01/02_Exercises/05_run.sh modified:   01/02_Exercises/Dockerfile2

--- 01/02_Exercises/04_scp.sh
+++ 01/02_Exercises/04_scp.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 #
 LIB='../../bin/aws.conf'
-#??
-#??
-#??
-#??
+if [ ! -f "$LIB" ]; then
+  echo "Error: Configuration file $LIB not found!"
+  exit 1
+fi
 source $LIB
 
 # Set variable for target directory where the app should be stored
-#??
+DIR="~/m347_exercises"
 
 # remove (with ssh) target directory for the app
 echo "Remove directory ${DIR} on ${DEST}"
-#??
+ssh -i "$KEY" "$DEST" "rm -rf ${DIR}"
 echo "Create directory ${DIR} on ${DEST}"
 # create (with ssh) target directory for the app
-#??
+ssh -i "$KEY" "$DEST" "mkdir -p ${DIR}"
 # copy (with scp) all relevant bash script & Dockerfiles to destination
-#??
-#??
-#??
+scp -i "$KEY" 05_run.sh 01_Basic.conf 02_Homedir.conf Dockerfile1 Dockerfile2 "${DEST}:${DIR}/"
+echo "Files copied successfully to ${DEST}:${DIR}/"
--- 01/02_Exercises/05_run.sh
+++ 01/02_Exercises/05_run.sh
@@ -1,50 +1,52 @@
 #!/bin/bash
 #
 # Import either 01_Basic.conf or 01_Homedir.conf
-#??
+source 01_Basic.conf
 # If no start-argument has been provided to the bash script then ...
-#??
+if [ -z "$1" ]; then
   # print error message and exit script
   printf "%s\n" \
     "Error: Provide at least 1 argument:" \
     "(1): create container, (2): remove container" \
     "Exit script."
-#??
-#??
+  exit 1
+fi
 
 
 case $1 in
 1)
   # if docker file does not exist ..
-#??
-#??
+  if [ ! -f "$file" ]; then
     # print error message and exit script
-#??
-#??
-#??
+    echo "Error: Dockerfile $file not found!"
+    exit 1
+  fi
+  
   # Build a dockerfile with tag -t
   printf "Start building %s ..." ${file}
-#??
-#??
+  docker build -t "$image" -f "$file" .
+  echo "\nDone."
+  
   # List images
-#??
-#??
-#??
+  echo "\nListing Docker images:"
+  docker image ls
+  echo ""
+  
   # Run container based on its image
-#??
+  docker run -itd --rm --name "$container" "$image"
   # Start bash-shell in container
-#??
+  docker exec -it "$container" bash
   ;;
 
 2)
   # stop container
-#??
+  docker stop "$container"
   # remove image
-#??
+  docker rmi "$image"
   # Check if the image still exists by filtering all
   # existing images with removed image. When output is empty then
   # image has been successfully removed.
-#??
+  docker images | grep "$image"
   ;;
 *)
   echo "Incorrect choice entered!"
